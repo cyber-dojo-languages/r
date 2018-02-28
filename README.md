cbc2b908d691a85f8b32d62feedd6d6b5689cb8b
Jon Jagger <jon@jaggersoft.com>
2018-02-28 15:44:47 +0000

    [ci skip] bump year in license file

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ![Build Status](https://travis-ci.org/cyber-dojo-languages/r.svg?branch=master)
 
-Version=3.4.3
+[Version=3.4.3](https://github.com/cyber-dojo-languages/r/blob/master/check_version.sh)
--- check_version.sh
+++ check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2)
+readonly EXPECTED=$(cat README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
 readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/rbase sh -c 'Rscript --version 2>&1')
 
 if echo ${ACTUAL} | grep -q ${EXPECTED}; then
